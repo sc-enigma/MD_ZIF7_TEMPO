466f250d2de9ffea68b16fec9b69235888c4f8b4
sc-enigma <153753211+sc-enigma@users.noreply.github.com>
2024-02-19 15:37:46 +0700

    Add files via upload

--- system/mdrun.py
+++ system/mdrun.py
@@ -4,8 +4,13 @@ from datetime import datetime
 
 def run_md(path):
     if not os.path.exists(path):
-        return 
+        print('PATH DOES NOT EXIST')
+        return
     os.chdir(path)
+    '''if '/prod/' in path:
+        os.system('ln -s ../eql/confout.gro')
+        os.system('gmx_mpi grompp -f prod.mdp -c *.gro -p topol.top -o prod.tpr -maxwarn 1 -v')
+        tm.sleep(10)'''
     os.system('gmx_mpi mdrun -s *.tpr -v')
     while True: 
         tm.sleep(1)
@@ -14,7 +19,7 @@ def run_md(path):
     tm.sleep(10)
 
 def write_log(path):
-    log_filname = '/home/sc_enigma/Projects/MD_UIO66/system/log.txt'
+    log_filname = '/home/sc_enigma/Projects/MD_ZIF7/system/log.txt'
     lines = []
     if os.path.exists(log_filname):
         f = open(log_filname, 'r')
@@ -27,14 +32,19 @@ def write_log(path):
     for line in lines:
         f.write(line)
     f.close()
+    print(f'{current_time} gmx_mpi mdrun {path}\n')
 
 '''
-    '/home/sc_enigma/Projects/MD_UIO66/production/zif7_tempo_lpA/prod/',
-    '/home/sc_enigma/Projects/MD_UIO66/production/zif7_tempo_lpB/prod/',
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_lpA/prod/',
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_lpB/prod/',
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_npA/prod/',
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_npB/prod/'
 '''
 
-path_list = []
-tm.sleep(3000) # !!! TEMPORARY !!!
+path_list = [\
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_npA/prod/',
+    '/home/sc_enigma/Projects/MD_ZIF7/production/zif7_tempo_npB/prod/']
+
 for path in path_list:
     write_log(path)
     run_md(path)
