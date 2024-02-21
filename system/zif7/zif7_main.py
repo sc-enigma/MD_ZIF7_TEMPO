f4d5597a315961f4a8a187921cd1ca5140d7e8fe
sc-enigma <d.alimov.remote@gmail.com>
2024-02-21 16:08:35 +0700

    tmp

--- system/zif7/zif7_main.py
+++ system/zif7/zif7_main.py
@@ -22,12 +22,12 @@ alpha = 90.00 / 180 * np.pi
 beta  = 90.00 / 180 * np.pi
 gamma = 120.00 / 180 * np.pi
 
-a_np     = 23.948
-b_np     = 21.354
-c_np     = 16.349
-alpha_np = 90.28 / 180 * np.pi
-beta_np  = 93.28 / 180 * np.pi
-gamma_np = 108.41 / 180 * np.pi
+a_lp     = 23.948
+b_lp     = 21.354
+c_lp     = 16.349
+alpha_lp = 90.28 / 180 * np.pi
+beta_lp  = 93.28 / 180 * np.pi
+gamma_lp = 108.41 / 180 * np.pi
 
 bounds_a, bounds_b, bounds_c = [1.0, 3.0], [1.0, 3.0], [1.0, 3.0]
 
@@ -45,39 +45,36 @@ atoms = define_zif7_atom_names(atoms)
 # STEP 4.a. Write .gro and .mol2 files for large pore structure
 with open('__tmp/atoms_zif7_lp.pickle', 'wb') as handle:
     pickle.dump(atoms, handle, protocol=pickle.HIGHEST_PROTOCOL)
-write_gro_file(atoms, 'zif7_lp.gro', a, b, c, alpha, beta, gamma, bounds_a, bounds_b, bounds_c)
-write_mol2_file(atoms, 'zif7_lp.mol2', a, b, c, alpha, beta, gamma, skip_long_bonds=False)
+write_gro_file(atoms, 'zif7_np.gro', a, b, c, alpha, beta, gamma, bounds_a, bounds_b, bounds_c)
+write_mol2_file(atoms, 'zif7_np.mol2', a, b, c, alpha, beta, gamma, skip_long_bonds=False)
 
 # STEP 4.b. Write .gro file for nano pore structure
 atoms = transform_lattice(atoms,
                           a, b, c, alpha, beta, gamma,
-                          a_np, b_np, c_np, alpha_np, beta_np, gamma_np)
+                          a_lp, b_lp, c_lp, alpha_lp, beta_lp, gamma_lp)
 with open('__tmp/atoms_zif7_np.pickle', 'wb') as handle:
     pickle.dump(atoms, handle, protocol=pickle.HIGHEST_PROTOCOL)
-write_gro_file(atoms, 'zif7_np.gro', a_np, b_np, c_np, alpha_np, beta_np, gamma_np, bounds_a, bounds_b, bounds_c)
-write_mol2_file(atoms, 'zif7_np.mol2', a_np, b_np, c_np, alpha_np, beta_np, gamma_np, skip_long_bonds=False)
+write_gro_file(atoms, 'zif7_lp.gro', a_lp, b_lp, c_lp, alpha_lp, beta_lp, gamma_lp, bounds_a, bounds_b, bounds_c)
+write_mol2_file(atoms, 'zif7_lp.mol2', a_lp, b_lp, c_lp, alpha_lp, beta_lp, gamma_lp, skip_long_bonds=False)
 
 # STEP 5. Write .itp files
-mass, charge, bond_params, angle_params, dihedral_params = get_zif7_params_lp()
-
-skip_long_bonds = True
-write_atoms(atoms, charge, mass, 'ZIF', 'atoms.itp')
-write_bonds(atoms, bond_params, 'bonds_lp.itp',             skip_long_bonds)
-write_angles(atoms, angle_params, 'angles_lp.itp',          skip_long_bonds)
-write_dihedrals(atoms, dihedral_params, 'dihedrals.itp', skip_long_bonds)
-compose_itp_files(['atomtypes.itp',
-                   'moleculetype.itp', 'atoms.itp', 'bonds_lp.itp', 'angles_lp.itp', 'dihedrals.itp'], 'zif7_lp.itp')
-
 mass, charge, bond_params, angle_params, dihedral_params = get_zif7_params_np()
-
-skip_long_bonds = True
+skip_long_bonds = False
 write_atoms(atoms, charge, mass, 'ZIF', 'atoms.itp')
 write_bonds(atoms, bond_params, 'bonds_np.itp',             skip_long_bonds)
 write_angles(atoms, angle_params, 'angles_np.itp',          skip_long_bonds)
-write_dihedrals(atoms, dihedral_params, 'dihedrals.itp', skip_long_bonds)
+write_dihedrals(atoms, dihedral_params, 'dihedrals_np.itp', skip_long_bonds)
 compose_itp_files(['atomtypes.itp',
-                   'moleculetype.itp', 'atoms.itp', 'bonds_np.itp', 'angles_np.itp', 'dihedrals.itp'], 'zif7_np.itp')
+                   'moleculetype.itp', 'atoms.itp', 'bonds_np.itp', 'angles_np.itp', 'dihedrals_np.itp'], 'zif7_np.itp')
 
+mass, charge, bond_params, angle_params, dihedral_params = get_zif7_params_lp()
+skip_long_bonds = False
+write_atoms(atoms, charge, mass, 'ZIF', 'atoms.itp')
+write_bonds(atoms, bond_params, 'bonds_lp.itp',             skip_long_bonds)
+write_angles(atoms, angle_params, 'angles_lp.itp',          skip_long_bonds)
+write_dihedrals(atoms, dihedral_params, 'dihedrals_lp.itp', skip_long_bonds)
+compose_itp_files(['atomtypes.itp',
+                   'moleculetype.itp', 'atoms.itp', 'bonds_lp.itp', 'angles_lp.itp', 'dihedrals_lp.itp'], 'zif7_lp.itp')
 
 
 
--- system/zif7_tempo/zif7_tempo.py
+++ system/zif7_tempo/zif7_tempo.py
@@ -17,11 +17,18 @@ for atom_idx in range(len(atoms_tempo)):
     atoms_tempo[atom_idx].resid = 'TMP'
     atoms_tempo[atom_idx].atom_idx = atom_idx
  
-def put_tempo_in_lattice_A(atoms_zif7, atoms_tempo):
+def put_tempo_in_lattice(atoms_zif7, atoms_tempo, pore_idx):
+    def calc_dist(atoms_zif7, atoms_tempo, shift):
+        min_dist = 1e10
+        for atom_tempo in atoms_tempo:
+            for atom_zif7 in atoms_zif7:
+                dist = pow(sum(np.power(atom_tempo.r + shift - atom_zif7.r, 2)), 0.5)
+                min_dist = min(min_dist, dist)
+        return min_dist
     # Calculate cell center
     cell_center = np.zeros(3)
-    for atom_idx in [2362, 2468]:
-        cell_center += atoms_zif7[atom_idx].r / 2.0
+    for atom_idx in pore_idx:
+        cell_center += atoms_zif7[atom_idx].r / len(pore_idx)
 
     # Calculate TEMPO center
     tempo_center = np.zeros(3)
@@ -32,91 +39,39 @@ def put_tempo_in_lattice_A(atoms_zif7, atoms_tempo):
     for atom_idx in range(len(atoms_tempo)):
         atoms_tempo[atom_idx].r += cell_center - tempo_center
 
-    # Calculate minimal distance from TEMPO to cell
-    min_dist = 1e10
-    for atom_tempo in atoms_tempo:
-        for atom_zif7 in atoms_zif7:
-            dist = pow(sum(np.power(atom_tempo.r - atom_zif7.r, 2)), 0.5)
-            min_dist = min(min_dist, dist)
-    print(min_dist)
-    return atoms_zif7.copy(), atoms_tempo.copy()
-
-def put_tempo_in_lattice_B(atoms_zif7, atoms_tempo):
-    # Calculate cell center
-    cell_center = np.zeros(3)
-    for atom_idx in [1783, 2465]:
-        cell_center += atoms_zif7[atom_idx].r / 2.0
-
-    # Calculate TEMPO center
-    tempo_center = np.zeros(3)
-    for atom in atoms_tempo:
-        tempo_center += atom.r / len(atoms_tempo)
-        
-    # Translate TEMPO atoms inside cell
+    best_dist = 0.0
+    best_shift = np.zeros(3)
+    for dx in np.linspace(-2.0, 2.0, 5):
+        for dy in np.linspace(-2.0, 2.0, 5):
+            for dz in np.linspace(-2.0, 2.0, 5):
+                shift = np.array([dx, dy, dz])
+                dist = calc_dist(atoms_zif7, atoms_tempo, shift)
+                print(dist)
+                if dist > best_dist:
+                    best_dist = dist
+                    best_shift = shift
     for atom_idx in range(len(atoms_tempo)):
-        atoms_tempo[atom_idx].r += cell_center - tempo_center
+        atoms_tempo[atom_idx].r += best_shift
 
     # Calculate minimal distance from TEMPO to cell
-    min_dist = 1e10
-    for atom_tempo in atoms_tempo:
-        for atom_zif7 in atoms_zif7:
-            dist = pow(sum(np.power(atom_tempo.r - atom_zif7.r, 2)), 0.5)
-            min_dist = min(min_dist, dist)
-    print(min_dist)
+    print(calc_dist(atoms_zif7, atoms_tempo, np.zeros(3)), best_shift)
     return atoms_zif7.copy(), atoms_tempo.copy()
-    
-# STEP 1. Structure with large pores. Pore A ________________________________________________________________________
-a     = 22.989
-b     = 22.989
-c     = 15.763
-alpha = 90.00 / 180 * np.pi
-beta  = 90.00 / 180 * np.pi
-gamma = 120.00 / 180 * np.pi
+
+# STEP 1. Structure with narrow pores. Pore A ________________________________________________________________________
+a_np     = 22.989
+b_np     = 22.989
+c_np     = 15.763
+alpha_np = 90.00 / 180 * np.pi
+beta_np  = 90.00 / 180 * np.pi
+gamma_np = 120.00 / 180 * np.pi
 bounds_a, bounds_b, bounds_c = [1.0, 3.0], [1.0, 3.0], [1.0, 3.0]
-
-# Load data
-with open('../zif7/__tmp/atoms_zif7_lp.pickle', 'rb') as handle:
-    atoms_zif7_lp = pickle.load(handle)
-    
-# Save .gro file
-atoms_zif7_lp, atoms_tempo = put_tempo_in_lattice_A(atoms_zif7_lp, atoms_tempo)
-with open('__tmp/atoms_zif7_lpA.pickle', 'wb') as handle:
-    pickle.dump(atoms_zif7_lp, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open('__tmp/atoms_tempo_lpA.pickle', 'wb') as handle:
-    pickle.dump(atoms_tempo, handle, protocol=pickle.HIGHEST_PROTOCOL)
-atoms_zif7_tempo_lpA = atoms_zif7_lp.copy()
-atoms_zif7_tempo_lpA.extend(atoms_tempo.copy())
-write_gro_file(atoms_zif7_tempo_lpA, 'zif7_tempo_lpA.gro', a, b, c, alpha, beta, gamma, bounds_a, bounds_b, bounds_c)
-
-# STEP 2. Structure with large pores. Pore B ________________________________________________________________________
-# Load data
-with open('../zif7/__tmp/atoms_zif7_lp.pickle', 'rb') as handle:
-    atoms_zif7_lp = pickle.load(handle)
-    
-# Save .gro file
-atoms_zif7_lp, atoms_tempo = put_tempo_in_lattice_B(atoms_zif7_lp, atoms_tempo)
-with open('__tmp/atoms_zif7_lpB.pickle', 'wb') as handle:
-    pickle.dump(atoms_zif7_lp, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open('__tmp/atoms_tempo_lpB.pickle', 'wb') as handle:
-    pickle.dump(atoms_tempo, handle, protocol=pickle.HIGHEST_PROTOCOL)
-atoms_zif7_tempo_lpB = atoms_zif7_lp.copy()
-atoms_zif7_tempo_lpB.extend(atoms_tempo.copy())
-write_gro_file(atoms_zif7_tempo_lpB, 'zif7_tempo_lpB.gro', a, b, c, alpha, beta, gamma, bounds_a, bounds_b, bounds_c)
-
-# STEP 3. Structure with nano pores. Pore A ________________________________________________________________________
-a_np     = 23.948
-b_np     = 21.354
-c_np     = 16.349
-alpha_np = 90.28 / 180 * np.pi
-beta_np  = 93.28 / 180 * np.pi
-gamma_np = 108.41 / 180 * np.pi
 
 # Load data
 with open('../zif7/__tmp/atoms_zif7_np.pickle', 'rb') as handle:
     atoms_zif7_np = pickle.load(handle)
-
+    
 # Save .gro file
-atoms_zif7_np, atoms_tempo = put_tempo_in_lattice_A(atoms_zif7_np, atoms_tempo)
+atoms_zif7_np, atoms_tempo = put_tempo_in_lattice(atoms_zif7_np, atoms_tempo, [2362, 2468])
 with open('__tmp/atoms_zif7_npA.pickle', 'wb') as handle:
     pickle.dump(atoms_zif7_np, handle, protocol=pickle.HIGHEST_PROTOCOL)
 with open('__tmp/atoms_tempo_npA.pickle', 'wb') as handle:
@@ -125,13 +80,13 @@ atoms_zif7_tempo_npA = atoms_zif7_np.copy()
 atoms_zif7_tempo_npA.extend(atoms_tempo.copy())
 write_gro_file(atoms_zif7_tempo_npA, 'zif7_tempo_npA.gro', a_np, b_np, c_np, alpha_np, beta_np, gamma_np, bounds_a, bounds_b, bounds_c)
 
-# STEP 4. Structure with nano pores. Pore B ________________________________________________________________________
+# STEP 2. Structure with narrow pores. Pore B ________________________________________________________________________
 # Load data
 with open('../zif7/__tmp/atoms_zif7_np.pickle', 'rb') as handle:
     atoms_zif7_np = pickle.load(handle)
-
+    
 # Save .gro file
-atoms_zif7_np, atoms_tempo = put_tempo_in_lattice_B(atoms_zif7_np, atoms_tempo)
+atoms_zif7_np, atoms_tempo = put_tempo_in_lattice(atoms_zif7_np, atoms_tempo, [1783, 2465])
 with open('__tmp/atoms_zif7_npB.pickle', 'wb') as handle:
     pickle.dump(atoms_zif7_np, handle, protocol=pickle.HIGHEST_PROTOCOL)
 with open('__tmp/atoms_tempo_npB.pickle', 'wb') as handle:
@@ -139,3 +94,40 @@ with open('__tmp/atoms_tempo_npB.pickle', 'wb') as handle:
 atoms_zif7_tempo_npB = atoms_zif7_np.copy()
 atoms_zif7_tempo_npB.extend(atoms_tempo.copy())
 write_gro_file(atoms_zif7_tempo_npB, 'zif7_tempo_npB.gro', a_np, b_np, c_np, alpha_np, beta_np, gamma_np, bounds_a, bounds_b, bounds_c)
+
+# STEP 3. Structure with large pores. Pore A ________________________________________________________________________
+a_lp     = 23.948
+b_lp     = 21.354
+c_lp     = 16.349
+alpha_lp = 90.28 / 180 * np.pi
+beta_lp  = 93.28 / 180 * np.pi
+gamma_lp = 108.41 / 180 * np.pi
+
+# Load data
+with open('../zif7/__tmp/atoms_zif7_lp.pickle', 'rb') as handle:
+    atoms_zif7_lp = pickle.load(handle)
+
+# Save .gro file
+atoms_zif7_lp, atoms_tempo = put_tempo_in_lattice(atoms_zif7_lp, atoms_tempo, [2362, 2468])
+with open('__tmp/atoms_zif7_lpA.pickle', 'wb') as handle:
+    pickle.dump(atoms_zif7_lp, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open('__tmp/atoms_tempo_lpA.pickle', 'wb') as handle:
+    pickle.dump(atoms_tempo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+atoms_zif7_tempo_lpA = atoms_zif7_lp.copy()
+atoms_zif7_tempo_lpA.extend(atoms_tempo.copy())
+write_gro_file(atoms_zif7_tempo_lpA, 'zif7_tempo_lpA.gro', a_lp, b_lp, c_lp, alpha_lp, beta_lp, gamma_lp, bounds_a, bounds_b, bounds_c)
+
+# STEP 4. Structure with large pores. Pore B ________________________________________________________________________
+# Load data
+with open('../zif7/__tmp/atoms_zif7_lp.pickle', 'rb') as handle:
+    atoms_zif7_lp = pickle.load(handle)
+
+# Save .gro file
+atoms_zif7_lp, atoms_tempo = put_tempo_in_lattice(atoms_zif7_lp, atoms_tempo, [1783, 2465])
+with open('__tmp/atoms_zif7_lpB.pickle', 'wb') as handle:
+    pickle.dump(atoms_zif7_lp, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open('__tmp/atoms_tempo_lpB.pickle', 'wb') as handle:
+    pickle.dump(atoms_tempo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+atoms_zif7_tempo_lpB = atoms_zif7_lp.copy()
+atoms_zif7_tempo_lpB.extend(atoms_tempo.copy())
+write_gro_file(atoms_zif7_tempo_lpB, 'zif7_tempo_lpB.gro', a_lp, b_lp, c_lp, alpha_lp, beta_lp, gamma_lp, bounds_a, bounds_b, bounds_c)
